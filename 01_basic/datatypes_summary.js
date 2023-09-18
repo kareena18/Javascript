8fcd575be183d93126ae9bd567304bba08f6ffa6
Kareena Sahoo <78543220+kareena18@users.noreply.github.com>
2023-09-18 09:54:58 +0000

    datatypes summary

---
+++ 01_basic/datatypes_summary.js
@@ -0,0 +1,36 @@
+//Primitive
+//7 types: string, number, boolean, null, undefined, symbol, bigint 
+
+const score =100
+const scoreValue =100.3
+const isLoggedIn =false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+const bigNumber = 23897865654564158789n
+
+//Reference (Non Primitive)
+//Array, Objects, Functions
+const heros =["shaktiman","naagraj","doga"];
+let myObj ={
+    person_name: "kareena",
+    age: 21
+}
+const myFunction = function(){
+    console.log("hello world");
+}
+console.log(typeof score)
+console.log(typeof scoreValue)
+console.log(typeof isLoggedIn)
+console.log(typeof outsideTemp)
+console.log(typeof userEmail)
+console.log(typeof id)
+console.log(typeof bigNumber)
+
+console.log(typeof heros)
+console.log(typeof myObj)
+console.log(typeof myFunction)
