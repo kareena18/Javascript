31418d9b56a90e287c5adfd876834d497b3114fd
Kareena Sahoo <78543220+kareena18@users.noreply.github.com>
2023-09-18 09:04:20 +0000

    js datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all js code as newer version
+//alert(3+3)  // we are using nodejs,not browser
+let person_name = "kareena";
+let age=21
+let isLoggedIn = false
+let state;
+
+//number bigint string boolean null undefined symbol(unique)
+//object
+console.log(typeof person_name)
+console.log(typeof age)
+console.log(typeof isLoggedIn)
+console.log(typeof state)
+
+console.log(typeof null)
+console.log(typeof undefined)
