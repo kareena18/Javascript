3172f6a536843d2f53ca4f000e99b8dfa2b2dc3d
Kareena Sahoo <78543220+kareena18@users.noreply.github.com>
2023-09-18 09:29:12 +0000

    operation in js

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -21,3 +21,40 @@ let someNumber=33
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+//********** Operations ***************
+
+let value =3
+let negvalue = -value
+console.log(negvalue)
+
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2*2)
+//console.log(2**2)
+//console.log(2/3)
+//console.log(2%3)
+
+let str1="hello"
+let str2=" kareena"
+let str3 = str1+str2
+console.log(str3)
+
+console.log("1"+2)//12
+console.log(1+"2")//12
+console.log("1"+2+2)//122
+console.log(1+2+"2")//32
+
+console.log((3+4)*5%3)//2
+console.log(true)//true
+console.log(+true)//1
+console.log(+"")//0
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter)
+++gameCounter;
+console.log(gameCounter)
