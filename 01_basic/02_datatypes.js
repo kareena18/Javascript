65ff04505e0e920fad731de585ea55c3dbe1e0fb
Kareena Sahoo <78543220+kareena18@users.noreply.github.com>
2023-09-20 06:51:58 +0000

    Datatypes in js

--- 01_basic/02_datatypes.js
+++ 01_basic/02_datatypes.js
@@ -1,12 +1,14 @@
 "use strict"; //treat all js code as newer version
 //alert(3+3)  // we are using nodejs,not browser
+
 let person_name = "kareena";
-let age=21
-let isLoggedIn = false
+let age = 21;
+let isLoggedIn = false;
 let state;
 
 //number bigint string boolean null undefined symbol(unique)
 //object
+
 console.log(typeof person_name)
 console.log(typeof age)
 console.log(typeof isLoggedIn)
