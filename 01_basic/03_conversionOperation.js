92381e2b4b4c8d0128e3b4a1fc7f70b49c9ce60b
Kareena Sahoo <78543220+kareena18@users.noreply.github.com>
2023-09-20 06:53:20 +0000

    Conversion and operation in js

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,6 +1,6 @@
-let score ="33abc"
+let score = "33abc"
 console.log(typeof score)
-console.log(typeof(score))
+console.log(typeof (score))
 
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber)
@@ -10,21 +10,21 @@ console.log(valueInNumber)
 //true=1;false=0
 //null=0 undefined=NaN
 
-let isLoggedIn=""
+let isLoggedIn = ""
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn);
 //1=true; 0=false
 //""=false
 //"kareena"=true
 
-let someNumber=33
+let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
 
 //********** Operations ***************
 
-let value =3
+let value = 3
 let negvalue = -value
 console.log(negvalue)
 
@@ -35,25 +35,25 @@ console.log(negvalue)
 //console.log(2/3)
 //console.log(2%3)
 
-let str1="hello"
-let str2=" kareena"
-let str3 = str1+str2
+let str1 = "hello"
+let str2 = " kareena"
+let str3 = str1 + str2
 console.log(str3)
 
-console.log("1"+2)//12
-console.log(1+"2")//12
-console.log("1"+2+2)//122
-console.log(1+2+"2")//32
+console.log("1" + 2)//12
+console.log(1 + "2")//12
+console.log("1" + 2 + 2)//122
+console.log(1 + 2 + "2")//32
 
-console.log((3+4)*5%3)//2
+console.log((3 + 4) * 5 % 3)//2
 console.log(true)//true
 console.log(+true)//1
 console.log(+"")//0
 
-let num1,num2,num3
-num1=num2=num3=2+2
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
 
-let gameCounter=100
+let gameCounter = 100
 gameCounter++;
 console.log(gameCounter)
 ++gameCounter;
