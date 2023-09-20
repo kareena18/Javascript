b3bb65fdaa61ff3b169049abe55941e21c495aeb
Kareena Sahoo <78543220+kareena18@users.noreply.github.com>
2023-09-20 05:38:13 +0000

    Arrays in js

---
+++ 02_basic/02_arrays.js
@@ -0,0 +1,26 @@
+const marvel_heros =["thar","ironman","spiderman"];
+const dc_heros =["superman","flash","batman"];
+
+//marvel_heros.push(dc_heros);
+//console.log(marvel_heros);
+//console.log(marvel_heros[3][1]);
+
+//const allHeros = marvel_heros.concat(dc_heros);
+//console.log(allHeros);
+
+const allNewHeros =[...marvel_heros,...dc_heros];
+console.log(allNewHeros);
+
+const anotherArray=[1,2,,3,4,5,6,7,[6,7,[4,5]]];
+
+const realAnotherArray = anotherArray.flat(Infinity);
+console.log(realAnotherArray);
+
+console.log(Array.isArray("kareena"));
+console.log(Array.from("kareena"));
+console.log(Array.from({name:"kareena"}));
+
+let score1=100;
+let score2=200;
+let score3=300;
+console.log(Array.of(score1,score2,score3));
