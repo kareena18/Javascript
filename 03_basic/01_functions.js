3391eb7787dd76e8c8b993523933ae7c58bc796c
Kareena Sahoo <78543220+kareena18@users.noreply.github.com>
2023-09-20 07:03:35 +0000

    Functions in js

--- 03_basic/01_functions.js
+++ 03_basic/01_functions.js
@@ -1,4 +1,4 @@
-function sayMyFunction(){
+function sayMyFunction() {
     console.log("K");
     console.log("A");
     console.log("R");
@@ -9,21 +9,21 @@ function sayMyFunction(){
 }
 //sayMyFunction();
 
-function addTwoNumbers(num1,num2){
-    console.log(num1+num2);
+function addTwoNumbers(num1, num2) {
+    console.log(num1 + num2);
 }
-function addTwoNum(num1,num2){
-    return num1+num2;
+function addTwoNum(num1, num2) {
+    return num1 + num2;
     console.log("kareena");
 }
-addTwoNumbers(1,"a");
- const result = addTwoNum(1,4);
-addTwoNumbers(1,null);
+addTwoNumbers(1, "a");
+const result = addTwoNum(1, 4);
+addTwoNumbers(1, null);
 
-console.log("result:",result);
+console.log("result:", result);
 
-function logInUserMessage(userName="sam"){
-    if(userName===undefined){
+function logInUserMessage(userName = "sam") {
+    if (userName === undefined) {
         console.log("please enter a username");
         return;
     }
@@ -32,26 +32,26 @@ function logInUserMessage(userName="sam"){
 console.log(logInUserMessage("kareena"));
 console.log(logInUserMessage());
 
-function calculateCarPrice(val1,val2,...num1){
+function calculateCarPrice(val1, val2, ...num1) {
     return num1;
 }
 console.log(calculateCarPrice(200));
-console.log(calculateCarPrice(200,400,500,2000));
+console.log(calculateCarPrice(200, 400, 500, 2000));
 
-const user1={
-    username:"kareena",
-    price:100
+const user1 = {
+    username: "kareena",
+    price: 100
 }
-function handleObject(anyObject){
+function handleObject(anyObject) {
     console.log(`useername is ${anyObject.username} and price is ${anyObject.price}`);
 }
 handleObject(user1);
 handleObject({
-    username:"sam",
-    price:399
+    username: "sam",
+    price: 399
 })
-const myNewArray=[200,400,100,600];
-function returnSecondValue(getArray){
+const myNewArray = [200, 400, 100, 600];
+function returnSecondValue(getArray) {
     return getArray[1];
 }
 console.log(returnSecondValue(myNewArray));
