995af7fc6d3683d4aa7d4f26e696c61fc7e49d51
Kareena Sahoo <78543220+kareena18@users.noreply.github.com>
2023-09-20 06:57:33 +0000

    Dates in js

--- 01_basic/07_dates.js
+++ 01_basic/07_dates.js
@@ -18,13 +18,13 @@ console.log(myCreatedDate.toLocaleString());
 let myTimeStamp = Date.now();
 console.log(myTimeStamp);
 console.log(myCreatedDate.getTime());
-console.log(Math.floor(Date.now()/1000))
+console.log(Math.floor(Date.now() / 1000))
 
 let newDate = new Date();
 console.log(newDate);
 console.log(newDate.getMonth());
 console.log(newDate.getDay());
 
-newDate.toLocaleString('default',{
+newDate.toLocaleString('default', {
     weekday: "long",
 });
