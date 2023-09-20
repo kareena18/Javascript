33379f6eda670c6b78f483deddc14e8a1c208eaa
Kareena Sahoo <78543220+kareena18@users.noreply.github.com>
2023-09-20 06:50:36 +0000

    Variables in js

--- 01_basic/01_variables.js
+++ 01_basic/01_variables.js
@@ -1,12 +1,12 @@
 const accountId = 123456;
 let accountEmail = "kareena@google.com";
-var accountPassword ="12345";
+var accountPassword = "12345";
 accountCity = "Jaipur";
 let accountState;
 
 //account =2;
-accountEmail="hc@google.com";
-accountPassword="234";
-accountCity="Pune";
+accountEmail = "hc@google.com";
+accountPassword = "234";
+accountCity = "Pune";
 console.log(accountId);
-console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
