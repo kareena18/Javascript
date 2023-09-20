09e3a1b481a3b790e8f109a65d22262e26df523c
Kareena Sahoo <78543220+kareena18@users.noreply.github.com>
2023-09-20 07:01:17 +0000

    Arrays in js

--- 02_basic/02_arrays.js
+++ 02_basic/02_arrays.js
@@ -1,5 +1,5 @@
-const marvel_heros =["thar","ironman","spiderman"];
-const dc_heros =["superman","flash","batman"];
+const marvel_heros = ["thar", "ironman", "spiderman"];
+const dc_heros = ["superman", "flash", "batman"];
 
 //marvel_heros.push(dc_heros);
 //console.log(marvel_heros);
@@ -8,19 +8,19 @@ const dc_heros =["superman","flash","batman"];
 //const allHeros = marvel_heros.concat(dc_heros);
 //console.log(allHeros);
 
-const allNewHeros =[...marvel_heros,...dc_heros];
+const allNewHeros = [...marvel_heros, ...dc_heros];
 console.log(allNewHeros);
 
-const anotherArray=[1,2,,3,4,5,6,7,[6,7,[4,5]]];
+const anotherArray = [1, 2, , 3, 4, 5, 6, 7, [6, 7, [4, 5]]];
 
 const realAnotherArray = anotherArray.flat(Infinity);
 console.log(realAnotherArray);
 
 console.log(Array.isArray("kareena"));
 console.log(Array.from("kareena"));
-console.log(Array.from({name:"kareena"}));
+console.log(Array.from({ name: "kareena" }));
 
-let score1=100;
-let score2=200;
-let score3=300;
-console.log(Array.of(score1,score2,score3));
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+console.log(Array.of(score1, score2, score3));
