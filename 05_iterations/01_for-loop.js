bb9608df8cc2edf412a8170898eee7e74c355b13
Kareena Sahoo <78543220+kareena18@users.noreply.github.com>
2023-09-21 09:07:41 +0000

    For loop in js

---
+++ 05_iterations/01_for-loop.js
@@ -0,0 +1,36 @@
+//for
+for (let i = 0; i <= 10; i++) {
+    const element = i; 
+    if(element==5)
+        console.log("5 is best number");
+    console.log(element);
+}
+//console.log(element)
+for (let i = 0; i <= 10; i++) {
+    console.log(`outer loop value ${i}`);
+    for(let j=0;j<=10;j++){
+        //console.log(`inner loop value ${j} and inner loop ${i}`);
+        console.log(`${i}*${j}= ${i*j}`);
+    }
+}
+
+let myArray=["flash","batman","superman"];
+for(let i=0;i<myArray.length;i++){
+    console.log(myArray[i]);
+}
+
+//break and continue
+for(let i=1;i<=20;i++){
+    if(i==5){
+        console.log(`detected 5`);
+        break;
+    }
+    console.log(`value of i is ${i}`);
+}
+for(let i=1;i<=20;i++){
+    if(i==5){
+        console.log(`detected 5`);
+        continue;
+    }
+    console.log(`value of i is ${i}`);
+}
